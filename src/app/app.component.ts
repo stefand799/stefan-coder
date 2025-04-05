7e9003e17005a02c41a3dfed81913ac7b03590aa
stefan_d799 <dinustefan1996@yahoo.com>
2025-04-05 21:43:27 +0300

    I added the about-me component and make it look good on different scales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { AboutMeComponent } from "./components/about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, HomePageComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, HomePageComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

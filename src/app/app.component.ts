839cad1c1903a8076c3f834640a693d85a59b3ec
stefan_d799 <dinustefan1996@yahoo.com>
2025-04-05 23:31:45 +0300

    I added images to education component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutMeComponent } from "./components/about-me/about-me.component";
+import { EducationComponent } from "./components/education/education.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, HomePageComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, HomePageComponent, AboutMeComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
